e8996f0405e587129ad2afaa0164658edbb78993
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-02-09 23:56:33 +0600

    Written a report to the project

--- assignment_1/mosaic.m
+++ assignment_1/mosaic.m
@@ -7,7 +7,7 @@ close all ; clc; clear; % Close all figure windows, Clear the screen and the wor
 
 tic; % start time
 
-newImg = create_mosaic('david-hellmann.jpg','single color',16,'circular');
+newImg = create_mosaic('mandrill.png','dataset',16,'circular');
 
 imshow(newImg);
 
