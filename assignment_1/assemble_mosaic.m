7e791955a0b065063ab1938d4b9fbc80810395e1
Anuar's mac <anuar.maratkhan@nu.edu.kz>
2018-02-09 22:52:17 +0600

    Refactored the code

--- assignment_1/assemble_mosaic.m
+++ assignment_1/assemble_mosaic.m
@@ -1,6 +1,6 @@
 function [ mosaic ] = assemble_mosaic( batches, hor_batches, ver_batches )
 %ASSEMBLE_MOSAIC the method generates single RGB image (matrix) from an
-%array of RGB image batches
+% array of RGB image batches
 %   Input:
 %       batches = array of RGB image batches
 %       hor_batches = number of horizontal batches in the image
@@ -8,22 +8,22 @@ function [ mosaic ] = assemble_mosaic( batches, hor_batches, ver_batches )
 %   Output:
 %       mosaic = final assembled RGB image
 
-batch_size = size(batches);
-horizontal = [];
-id = 1;
+    batch_size = size(batches);
+    horizontal = [];
+    id = 1;
 
-for i = 1:hor_batches
-    vertical = reshape(batches(id,:,:,:), [batch_size(2) batch_size(3) 3]);
-    id = id+1;
-    for j = 1:ver_batches-1
-        reshaped_batch = reshape(batches(id,:,:,:), [batch_size(2) batch_size(3) 3]);
+    for i = 1:hor_batches
+        vertical = reshape(batches(id,:,:,:), [batch_size(2) batch_size(3) 3]);
         id = id+1;
-        vertical = cat(1, vertical, reshaped_batch);
+        for j = 1:ver_batches-1
+            reshaped_batch = reshape(batches(id,:,:,:), [batch_size(2) batch_size(3) 3]);
+            id = id+1;
+            vertical = cat(1, vertical, reshaped_batch);
+        end
+        horizontal = cat(2, horizontal, vertical);
     end
-    horizontal = cat(2, horizontal, vertical);
-end
 
-mosaic = horizontal;
+    mosaic = horizontal;
 
 end
 
--- assignment_1/euclidean_distance.m
+++ assignment_1/euclidean_distance.m
@@ -1,13 +1,18 @@
 function [ euc_dist ] = euclidean_distance( avg1, avg2 )
-%EUCLIDEAN_DISTANCE Summary of this function goes here
-%   Detailed explanation goes here
+%EUCLIDEAN_DISTANCE The method calculates euclidean distance of two
+% vectors
+%   Input:
+%       avg1 = first vector containing average RGB values
+%       avg2 = second vector containing average RGB values
+%   Output:
+%       euc_dist = euclidean distance of provided vectors
 
 
-r = (avg1(1) - avg2(1))^2;
-g = (avg1(2) - avg2(2))^2;
-b = (avg1(3) - avg2(3))^2;
+    r = (avg1(1) - avg2(1))^2;
+    g = (avg1(2) - avg2(2))^2;
+    b = (avg1(3) - avg2(3))^2;
 
-euc_dist = sqrt(r+g+b);
+    euc_dist = sqrt(r+g+b);
 
 end
 
